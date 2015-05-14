b05313fac14fb19ccb6153d0a69107c2791578f1
Shannon Byrne <s.byrne2254@gmail.com>
2015-05-14 11:45:26 -0500

    Some more changes to twilio auth

--- ruby.rb
+++ ruby.rb
@@ -1,33 +1,33 @@
 require 'rubygems'
-require 'twilio-ruby'
 require 'sinatra'
+require 'twilio-ruby'
 
 get '/' do
-    'Hello World! Currently running version ' + Twilio::VERSION + \
-    ' of the twilio-ruby library.'
+  'Hello World! Currently running version ' + Twilio::VERSION + \
+  ' of the twilio-ruby library.'
 end
 
-get '/send_message' do
-    account_sid = ENV['TWILIO_SID']
-    auth_token = ENV['TWILIO_AUTH']
-    client = Twilio::REST::Client.new account_sid, auth_token
-    
-    from = "+18327421825" # Your Twilio number
-    
-    friends = {
-        "+18324658840" => "Shannon",
-        "+15205088375" => "Lakeida"
-    }
-    friends.each do |key, value|
-        client.account.messages.create(
-                                       :from => from,
-                                       :to => key,
-                                       :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
-                                       )
-                                       puts "Sent message to #{value}"
-    end
-end
+get '/test_message' do
+  account_sid = ENV['TWILIO_SID']
+  auth_token = ENV['TWILIO_AUTH']
+  client = Twilio::REST::Client.new(account_sid, auth_token)
 
-get '/incoming_message_example' do
+  from = "+18327421825"
+
+  friends = {
+      "+18324658840" => "Shannon",
+      "+15205088375" => "Lakeida"
+  }
+  friends.each do |key, value|
+    puts "key"
+    puts "value"
+    client.account.messages.create(
+        :from => from,
+        :to => key,
+        :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
+    )
+  end
+
+  "Texts sent"
 
 end
